4b4c10908b8d03b64d7afb44e3109c123e5fdc49
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2023-01-26 08:45:59 +0100

    Added KBD and Indicators

--- build.js
+++ build.js
@@ -213,3 +213,105 @@ fs.writeFile('snippets/buttons.json', JSON.stringify(items, null, 2), function (
 	if (err) return console.log(err);
 	console.log('Created ' + Object.keys(items).length + ' snippets for buttons');
 });
+
+var indicators = {
+	"indicator-count": {
+		"scope": "html",
+		"prefix": "flowbite-indicator-count",
+		"body": "<span class=\"inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-blue-600 rounded-full\">1</span>",
+		"description": "Flowbite indicator count"
+	},
+	"indicator-loader": {
+		"scope": "html",
+		"prefix": "flowbite-indicator-loader",
+		"body": `<div class="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"><div class="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">loading...</div></div>`,
+		"description": "Flowbite indicator loader"
+	},
+
+	"indicator-loader-spinner": {
+		"scope": "html",
+		"prefix": "flowbite-indicator-spinner",
+		"body": `<div class="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"><div role="status"><svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg><span class="sr-only">Loading...</span></div></div>`,
+		"description": "Flowbite indicator spinner"
+	},
+	"indicator-stepper":{
+		"scope": "html",
+		"prefix": "flowbite-indicator-stepper",
+		"body": `<ol class="flex items-center"><li class="relative w-full mb-6"><div class="flex items-center"><div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-600 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0"><svg aria-hidden="true" class="w-4 h-4 text-blue-100 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg></div><div class="flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div></div><div class="mt-3"><h3 class="font-medium text-gray-900 dark:text-white">Step 1</h3></div></li><li class="relative w-full mb-6"><div class="flex items-center"><div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-600 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0"><svg aria-hidden="true" class="w-4 h-4 text-blue-100 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg></div><div class="flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div></div><div class="mt-3"><h3 class="font-medium text-gray-900 dark:text-white">Step 2</h3></div></li><li class="relative w-full mb-6"><div class="flex items-center"><div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-600 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0"><svg aria-hidden="true" class="w-4 h-4 text-blue-100 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg></div><div class="flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div></div><div class="mt-3"><h3 class="font-medium text-gray-900 dark:text-white">Step 2</h3></div></li><li class="relative w-full mb-6"><div class="flex items-center"><div class="z-10 flex items-center justify-center w-6 h-6 bg-gray-200 rounded-full ring-0 ring-white dark:bg-gray-700 sm:ring-8 dark:ring-gray-900 shrink-0"><svg aria-hidden="true" class="w-3 h-3 text-gray-800 dark:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></div></div><div class="mt-3"><h3 class="font-medium text-gray-900 dark:text-white">Step 3</h3></div></li></ol>`,
+		"description": "Flowbite indicator stepper"
+	},
+	"indicator-status":{
+		"scope": "html",
+		"prefix": "flowbite-indicator-status",
+		"body":`<div class="relative"><img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="profile image"><span class="top-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span></div>`,
+		"description": "Flowbite indicator status"
+	}
+};
+
+// Indicators
+const indicatorOptionsColors = [
+	{
+		label: 'white',
+		color: 'bg-gray-200'
+	},
+	{
+		label: 'gray',
+		color: 'bg-gray-900 dark:bg-gray-700'
+	},
+	{
+		label: 'blue',
+		color: 'bg-blue-600'
+	},
+	{
+		label: 'green',
+		color: 'bg-green-500'
+	},
+	{
+		label: 'red',
+		color: 'bg-red-500'
+	},
+	{
+		label: 'purple',
+		color: 'bg-purple-500'
+	},
+	{
+		label: 'indigo',
+		color: 'bg-indigo-500'
+	},
+	{
+		label: 'yellow',
+		color: 'bg-yellow-300'
+	},
+	{
+		label: 'teal',
+		color: 'bg-teal-500'
+	}
+];
+
+indicatorOptionsColors.forEach((indicatorOption) => {
+	indicators[indicatorOption.label] = {
+		scope: "html",
+		prefix: "flowbite-indicator-" + indicatorOption.label,
+		body: `<span class="flex w-3 h-3 rounded-full ${indicatorOption.color}"></span>`,
+		description: "Flowbite indicator " + indicatorOption.label
+	};
+
+	indicators[indicatorOption.label + "-with-legend"] = {
+		scope: "html",
+		prefix: "flowbite-indicator-" + indicatorOption.label + "-with-legend",
+		body: `<span class="flex items-center text-sm font-medium text-gray-900 dark:text-white"><span class="flex w-2.5 h-2.5 ${indicatorOption.color} rounded-full mr-1.5 flex-shrink-0"></span>Legend</span>`,
+		description: "Flowbite indicator " + indicatorOption.label + " with legend"
+	};
+})
+
+indicators['indicator-count'] = {
+	scope: "html",
+	prefix: "flowbite-indicator-count",
+	body: `<button type="button" class="relative inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5 mr-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg><span class="sr-only">Notifications</span>Messages<div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">8</div></button>`,
+	description: "Flowbite indicator count"
+}
+
+fs.writeFile('snippets/indicators.json', JSON.stringify(indicators, null, 2), function (err) {
+	if (err) return console.log(err);
+	console.log('Created ' + Object.keys(indicators).length + ' snippets for indicators');
+})
